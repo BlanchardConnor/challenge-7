8114500622a010efa9069ea8a08ec7babcda317c
BlanchardConnor <BlanchardConnor73@gmail.com>
2023-05-19 14:51:09 -0500

    added basic objects to index.js file. started writing arrays for question logs

--- index.js
+++ index.js
@@ -1,7 +1,78 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title!');
+                return false;
+            }
+        }  
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the title of your project? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please provide a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
